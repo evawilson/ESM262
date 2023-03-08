3a10edf0b52b4ab6605b7a21ded42797bc580e19
evawilson <evawilson@ucsb.edu>
2023-03-07 18:40:49 -0800

    Assignment 4

---
+++ R/classify_fish.R
@@ -0,0 +1,16 @@
+classify_fish
+#'@param fish name (salmon, tuna, steelhead, cod, shark)
+
+classify_fish = function(fish_name) {
+  result = case_when(fish_name = 50 )
+
+    
+    return(result)
+    }
+
+
+Store as a vector of numbers, then calculate  max and min 
+
+1. the most common fish,
+2. the rarest fish
+3. the total number of fish (count?)
