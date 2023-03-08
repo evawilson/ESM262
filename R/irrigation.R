b3378365768e2a6e7bd3f44aa7f2c258daca7a69
n-mcmanus <nmcmanus@bren.ucsb.edu>
2023-03-08 11:58:08 -0800

    irrigation time and amount functions

--- R/irrigation.R
+++ R/irrigation.R
@@ -4,8 +4,28 @@
 #' @param evapotranspiration (mm)
 
 irrigation=function(precipitation,evapotranspiration) {
-  result = 
-    return(result)
+  
+  ### how many days after rain event should you irrigate?
+  irrigation_time = if (ET - PE <= 5) {
+    0
+  } else if (ET - PE <= 10) {
+    1
+  } else if (ET - PE <= 15) {
+    2
+  } else if (ET - PE <= 20) {
+    3
+  } else if (ET - PE <= 25) {
+    4
+  } else {5}
+  
+  ### how much should you irrigate?
+  irrigation_amount = case_when((crop_type == 'almonds') ~ 5,
+                                (crop_type == 'cotton') ~ 3,
+                                (crop_type == 'vineyard') ~ 1)
+
+  
+  return(list(irrigation_time, irrigation_amount))
+  
 }
 
 Outputs:
