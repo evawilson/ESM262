2057975985e24f62df362879e8a3695181681565
evawilson <evawilson@ucsb.edu>
2023-03-09 11:48:31 -0800

    edited tests

--- tests/irrigation_works.R
+++ tests/irrigation_works.R
@@ -1,6 +1,4 @@
 test_that("irrigation_works", {
-# source my function  
-here("irrigation.R")
   expect_equal(irrigation_time(PE = 6, ET = 10), 0) 
   expect_equal(irrigation_time(PE = 4, ET = 10), 1)
 })
