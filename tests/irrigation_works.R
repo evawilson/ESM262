bd53e97854ab0fc976cadea55d654dcf1d385b1d
evawilson <evawilson@ucsb.edu>
2023-03-09 11:47:26 -0800

    Adding testing

--- tests/irrigation_works.R
+++ tests/irrigation_works.R
@@ -1,3 +1,6 @@
 test_that("irrigation_works", {
-  expect
+# source my function  
+here("irrigation.R")
+  expect_equal(irrigation_time(PE = 6, ET = 10), 0) 
+  expect_equal(irrigation_time(PE = 4, ET = 10), 1)
 })
