e6d4bda772264e820cbd8e892bcde19d2d15e4a3
n-mcmanus <nmcmanus@bren.ucsb.edu>
2023-03-10 12:36:10 -0800

    functional pmap and tidy results!

--- R/irrigation.R
+++ R/irrigation.R
@@ -1,39 +1,35 @@
-#' irrigation 
+#' irrigation function
 #' 
-#' @param precip (PE) (mm/day)
+#' @param precipitation (precip) (mm/day)
 #' @param evapotranspiration (ET) (mm/day)
-#' @param crop_type type (wheat, corn, barley)
-#' @returns how many days after rain event irrigation should take place and how much the crops should be irrigated
+#' @param crop_type (wheat, corn, barley)
+#' @returns list of relative soil moisture availability, whether that crop should be irrigated, and how much water should be given (in L).
 
 irrigation=function(precip, ET, crop_type) {
   
   ### start with some error checking - we should never have negative ET or PE values
-  if (length(ET) < 0)
-    return("ET value is negative")
-  
-  if (length(precip) < 0)
-    return("PE value is negative")
+  # if (length(ET) < 0)
+  #   return("ET value is negative")
+  # 
+  # if (length(precip) < 0)
+  #   return("PE value is negative")
   
   
   ### use PE and ET as proxy for available soil moisture
   soil_moisture_avail = precip - ET
   
   ### how many days after rain event should you irrigate?
-  should_irrigate = if (soil_moisture_avail < 0) {
-    FALSE
-  } else {TRUE}
+  should_irrigate = ifelse (soil_moisture_avail < 0, "No", "Yes") 
   
   ### how much should you irrigate?
-  irrigation_amount = if (should_irrigate == TRUE) {
-    case_when((crop_type == 'wheat') ~ 5,
-              (crop_type == 'corn') ~ 3,
-              (crop_type == 'barley') ~ 1)
+  irrigation_amount = case_when((crop_type == 'wheat' & should_irrigate == "Yes") ~ 5,
+                                (crop_type == 'corn' & should_irrigate == "Yes") ~ 3,
+                                (crop_type == 'barley' & should_irrigate == "Yes") ~ 1,
+                                (should_irrigate == "No") ~ 0)
     
-  } else {0}
-                          
-                          
-
-  ## return two outputs
+    
+  
+  ## return outputs as list
   return(list(soil_moisture_avail, should_irrigate, irrigation_amount))
   
 }
