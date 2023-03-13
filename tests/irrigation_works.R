577f77b5385af96b1350e983adda5c9bf3997c39
kmoya7 <kmoya@bren.ucsb.edu>
2023-03-12 21:08:28 -0700

    removed assignment instructions and added our names

--- tests/irrigation_works.R
+++ tests/irrigation_works.R
@@ -2,3 +2,4 @@ test_that("irrigation_works", {
   expect_equal(irrigation(precip = 2, ET = 3, crop_type = "barley"), list(-1, "No", 0))
   expect_equal(irrigation(precip = 10, ET = 3, crop_type = "barley"), list(7, "Yes", 1))
 })
+
