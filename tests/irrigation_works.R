bdc0671ed0521a617cd15c35f4119ec51ddf957e
n-mcmanus <nmcmanus@bren.ucsb.edu>
2023-03-12 20:34:45 -0700

    test works??

--- tests/irrigation_works.R
+++ tests/irrigation_works.R
@@ -1,4 +1,4 @@
 test_that("irrigation_works", {
-  expect_equal(irrigation(precip = 2, ET = 3, crop_type = "barley"), c(-1, "No", 0))
-  expect_equal(irrigation(precip = 10, ET = 3, crop_type = "barley"), c(7, "Yes", 1))
+  expect_equal(irrigation(precip = 2, ET = 3, crop_type = "barley"), list(-1, "No", 0))
+  expect_equal(irrigation(precip = 10, ET = 3, crop_type = "barley"), list(7, "Yes", 1))
 })
